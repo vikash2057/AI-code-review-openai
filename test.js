c05a34e4dc967edca4ba5655a8cf181e45f54312
Vikash Kumar <vikash2057@gmail.com>
2025-07-18 16:39:08 +0530

    Trigger AI Review Temp

---
+++ test.js
@@ -0,0 +1,7 @@
+email="sdfd"
+const fs = require('fs');
+const data = fs.readFileSync('data.txt');
+console.log(data.toString())
+const hash = location.hash.substring(1);
+document.getElementById("output").innerHTML = hash;
+console.log("123")
