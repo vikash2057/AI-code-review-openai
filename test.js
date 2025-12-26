e652740ce5b03c96ef7de9176f43fe569e508311
Vikash Kumar <vikash2057@gmail.com>
2025-12-26 10:47:20 +0530

    Trigger AI Review

--- test.js
+++ test.js
@@ -1,21 +1,5 @@
 const fs = require('fs');
-
-try {
-    const data = fs.readFileSync('data.txt');
-    console.log(data.toString());
-} catch (error) {
-    console.error('Error reading file:', error);
-}
+const data = fs.readFileSync('data.txt');console.log(data.toString());
 
 const hash = location.hash.substring(1);
-
-// Simple sanitization example: encode HTML entities
-function sanitize(input) {
-    return input.replace(/&/g, "&amp;")
-                .replace(/</g, "&lt;")
-                .replace(/>/g, "&gt;")
-                .replace(/"/g, "&quot;")
-                .replace(/'/g, "&#039;");
-}
-
 document.getElementById("output").innerHTML = sanitize(hash);
