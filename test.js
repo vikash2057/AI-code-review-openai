e0e68461548d58a5b6bc15d7dbdd88e3d61f7eb9
Vikash Kumar <vikash2057@gmail.com>
2025-12-30 12:42:06 +0530

    Trigger AI Review7

--- test.js
+++ test.js
@@ -1,6 +1,2 @@
-const fs = require('fs');
-const data = fs.readFileSync('data.txt');console.log(data.toString());
-
-const hash = location.hash.substring(1);
-document.getElementById("output").innerHTML = sanitize(hash);
-console.log("testttttttttttt222");
+const fileData = fs.readFileSync('data.txt');
+    console.log(fileData.toString());
